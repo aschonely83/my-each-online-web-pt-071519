966333e6f64c9af363a603dfc6bce11b9b31adf6
Adam Schonely <aschonely@gmail.com>
2019-07-26 00:50:20 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
-def my_each(array) # put argument(s) here
+def my_each(collection) # put argument(s) here
   i = 0 
   
-    while i < array.length do 
+    while i < collection.length do 
       i = i + 
-      yield(array[i])
+      yield(collection[i])
     end
     array
 end
